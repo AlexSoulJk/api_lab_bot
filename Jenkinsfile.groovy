1c3e01f491db682c12535811e78765c118cae767
AlexSoulJk <alex.solomatow2016@yandex.ru>
2024-06-02 22:37:19 +0300

    F11

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -52,7 +52,6 @@ pipeline {
                                 sh 'cp $CREDS ./api_lab_bot/credentials.json'
                                 sh 'cp $TOKEN ./api_lab_bot/token.json'
                             } else {
-//                                 bat 'powershell Copy-Item %ENV% -Destination ./api_lab_bot/.env'
                                 bat 'powershell Copy-Item %CREDS% -Destination ./api_lab_bot/credentials.json'
                                 bat 'powershell Copy-Item %TOKEN% -Destination ./api_lab_bot/token.json'
                             }
