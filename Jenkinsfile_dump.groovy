c460f07b775755b95626e11c794a98e4d7a90905
AlexSoulJk <alex.solomatow2016@yandex.ru>
2024-06-02 19:29:47 +0300

    F7

--- Jenkinsfile_dump.groovy
+++ Jenkinsfile_dump.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 script {
                     if (fileExists('backup_database.py')) {
-                        sh 'py backup_database.py'
+                        sh 'start /B python3 backup_database.py'
                     } else {
                         echo 'Файл backup_database.py не найден'
                     }
